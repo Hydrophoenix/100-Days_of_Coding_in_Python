9cb0103996ab28f5d63c2399516291cc868cb28f
Hydrophoenix <nkwochaugo2006@gmail.com>
2022-10-12 14:57:58 +0100

    day 1 of 100 days of code

---
+++ Day_1/inputing.py
@@ -0,0 +1,5 @@
+'''
+Write a program that prints the number of characters in a user's name
+'''
+username = input("Type in your name: ")
+print(len(username))
---
+++ Day_1/printing.py
@@ -0,0 +1,14 @@
+'''
+Write a program in main.py that prints the text below
+Day 1 - Python Print Function
+The function is declared like this:
+print('what to print')
+'''
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+print("\n")
+print("Day 1 - Python Print function\nThe function is declared like this:\nprint('what to print')")
+
+# ctrl + / is used for commenting a single line
