23d9719698f538e52088f8b0a78143fe5959bf27
Hydrophoenix <nkwochaugo2006@gmail.com>
2023-02-01 15:31:17 +0100

    Update tip_calculator.py

--- Day_2/tip_calculator.py
+++ Day_2/tip_calculator.py
@@ -13,8 +13,9 @@ members = int(input("How many people would be splitting the bill?: "))
 # Do the calculation each participant would pay from the total bill
 tip = percent_tip / 100 * bill # determining the actual tip
 total_bill = tip + bill # getting the total bill
-members_bill = round(total_bill / members, 2) # getting each participant bill
+members_bill = total_bill / members # getting each participant bill
+MEMBERS_BILL = "{:0.2f}".format(members_bill) # rounding the bill to 2 d.p
 
 # Display the result
-message = f"Each person would pay {members_bill}"
+message = f"Each person would pay {MEMBERS_BILL}"
 print(message)
