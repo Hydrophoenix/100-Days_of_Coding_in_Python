aebd21fe5709bdf4f93c0e5dd6f04b6c4b91de20
Hydrophoenix <nkwochaugo2006@gmail.com>
2023-02-01 15:31:07 +0100

    Update bmi_calculator.py

--- Day_2/bmi_calculator.py
+++ Day_2/bmi_calculator.py
@@ -14,9 +14,19 @@ user_height = float(input("Type in your height in meters (m): "))
 
 # Calculate the BMI
 user_BMI = user_weight / user_height**2
-
-# Display the result to the user using the round function
-print(f"{username} your BMI is {round(user_BMI, 2)}")
+user_BMI = round(user_BMI)
+# Display the result to the user using the round function and the interpretation of their BMI
+if user_BMI < 18.5:
+    print(f"{username}, your BMI is {user_BMI}, you are underweight")
+elif user_BMI <= 24.99:
+    print(f"{username}, your BMI is {user_BMI}, you have a normal weight")
+elif user_BMI <= 29.99:
+    print(f"{username}, your BMI is {user_BMI}, you are sligthly overweight")
+elif user_BMI <= 34.99:
+    print(f"{username}, your BMI is {user_BMI}, you are obese")
+else:
+    print(f"{username}, your BMI is {user_BMI}, you are clinically obese")
 
 # // is used to give the whole number of a division
 # % is used to give the remainder of a division exercise
+# The round fnc is used to round up a decimal
